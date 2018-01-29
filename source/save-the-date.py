0c1c7530161b38b5c73d7fb69ca9afd777c7b323
eliheuer <eliheuer@gmail.com>
2018-01-29 01:32:21 -0500

    Saving work state

--- source/save-the-date.py
+++ source/save-the-date.py
@@ -1,9 +1,6 @@
 # 72 dpi, 4" x 3"
 size(288, 216)
 
-# Debug tools
-# print installedFonts()
-
 # gird variables
 origin = (16, 16)
 height = 184
@@ -34,28 +31,29 @@ def grid(origin, width, height, num_x_units, num_y_units):
         step_col += (col + gut)
 
 
-fill(0.8, 0.8, 0.8)
-stroke(None)
-rect(0, 0, 288, 216)
+#fill(0.8, 0.8, 0.8)
+#stroke(None)
+#rect(0, 0, 288, 216)
 
 translate(*origin) # grid off
 #grid(origin, width, height, num_x_units*2, num_y_units*2) # grid on
 
 # Debug 
 x, y, w, h = 0, 0, ((col * 8)+(gut *7)), 173
-#fill(1, 0, 0, .1)
-#rect(x, y, w, h)
 
 font("fonts/Wilmette-Italic.otf")
+#font("fonts/Spectral-RegularItalic.otf")
 fill(0, 0, 0)
 stroke(None)
 
 # Line 1
-fontSize(48)
+tracking(0)
+fontSize(40)
 textBox("Save the Date",
     (x, y+7, w, h), align="center")
 
 # Line 2
+tracking(0)
 fontSize(24)
 textBox("Sarah Jones",
     (x, y-46, w, h), align="center")
@@ -70,20 +68,23 @@ fontSize(24)
 textBox("Eli  Heuer",
     (x, y-92, w, h), align="center")
 
-
+tracking(1)
 # Line 5
 fontSize(13)
 textBox("Are Getting Married",
     (x, y-117, w, h), align="center")
 
+tracking(0)
 # Line 6
 fontSize(13)
-textBox("❦",
-    (x, y-137, w, h), align="center")
+#❤
+#textBox("❦",
+#    (x, y-137, w, h), align="center")
 
+#openTypeFeatures(onum=True, smcp=True)
 # Line 7
 fontSize(13)
-textBox("8-18-18 • NYC • https://sarah-eli.wedding",
+textBox("8-18-18 • NYC • https://sarah-eli.com",
     (x, y-153, w, h), align="center")
 
 #saveImage("save-the-date.pdf")
